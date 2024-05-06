0b6a9194af10104ff92c7866b88fc1d8c65fca0d
Joseph Olarte <josephsantiago2345@gmail.com>
2024-05-06 13:32:31 -0500

    Primer commit

---
+++ CapaAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CapaAdmin.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ CapaTienda/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CapaTienda.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
